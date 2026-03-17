0bcbff1f9682b493f9f11b3d64ce2d5688337f95
Seyi Adeleye <seyiadel03@gmail.com>
2026-03-17 16:26:25 +0100

    fix: remove prematurely defined foreign key on role_id to allow migrations to run

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->boolean('is_active')->default(true);
             $table->uuid('market_id')->nullable();
-            $table->foreignUuid('role_id')->nullable()->constrained('roles')->onDelete('set null');
+            $table->uuid('role_id')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
